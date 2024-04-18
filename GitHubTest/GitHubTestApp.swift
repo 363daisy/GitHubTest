5e2483d553fa7a3d285a36352d94d56d4449f22f
363daisy <363daisy@gmail.com>
2024-04-18 20:01:11 +0900

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by NANA on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
